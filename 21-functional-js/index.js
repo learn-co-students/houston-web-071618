1a607ecfb1f6319a65a4b7156e66e08a7f6319ec
Tim Campbell <timothylevicampbell@gmail.com>
2018-09-04 14:09:06 -0500

    Update toy tale

---
+++ 21-functional-js/index.js
@@ -0,0 +1,28 @@
+// declaration
+function add1(num) {
+  return num + 1
+}
+
+// expression
+const add2 = function(num) {
+  return num + 2
+}
+// const someObj = {}
+
+// const log = function(arg1) {
+//   return function(arg2) {
+//     return function(arg3) {
+//       return function(arg4) {
+//         return function(arg5) {
+//           console.log(arg1, arg2, arg3, arg4, arg5);
+//         }
+//       }
+//     }
+//   }
+// }
+
+// function() {
+//   return {}
+// }
+
+// [1,2,3].map(function() {})
