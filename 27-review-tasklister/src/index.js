7495cfaf5a5787b63966d50c24c1a3dcb96b1263
Tim Campbell <timothylevicampbell@gmail.com>
2018-09-05 09:19:17 -0500

    Init TaskLister

---
+++ 27-review-tasklister/src/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // your solution here
+  // grab DOM elements
+  const listDiv = document.getElementById("app-content");
+
+  const app = new TaskLister();
+});
